885cc5f5144914cbbeb13db02b48deaa336579a0
deyesarr <69664183+deyesarr@users.noreply.github.com>
2022-02-19 00:38:15 -0800

    Add files via upload

---
+++ assign2/task1/build.sh
@@ -0,0 +1,4 @@
+intro.c: In function 'main':
+intro.c:11:3: warning: ignoring return value of 'scanf' declared with attribute 'warn_unused_result' [-Wunused-result]
+   11 |   scanf("%f", &r);
+      |   ^~~~~~~~~~~~~~~
