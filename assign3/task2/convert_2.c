adccba908c5c097f43d9ccbf61181ee3fb256b16
Ndeye Sarr <69664183+deyesarr@users.noreply.github.com>
2022-02-22 01:38:55 +0000

    task2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -6,33 +6,21 @@
 char* convert_2(int dec)
 {
 	//TODO: your implementation
- int arr= 32;
-    char *con;
-    con = (char*)malloc((arr+9)*sizeof(char)); 
-    con[0] = '0';
-    con[1] = 'b';
-    int located = arr + 8; //(arr+9)-1
-    int i = 1;
+int i, con, init;
+  char *bin;
+  init = 0;
+  bin = (char*)malloc(32+1);
 
-    while(located >= 2)
-    {
-        if(i == 0 ) { 
-                con[located] = ' ';
-                }
-                else {
-                if(dec&1) {  
-                                b[located] ='1';
-                        }
-                        else {
-                                b[located] ='0';
-                        }
-                dec >>= 1;        
-                } 
-            located--; 
-            i = (i+1)%5; 
-    }
-    return con;
-  free(con);
+  for (i = 31; i >= 0; i--){
+    con = dec >> i;
+    if (con & 1)
+      *(bin + init) = 1 + '0';
+    else
+      *(bin + init) = 0 + '0';
+    init++;
+  }
+  *(bin + init) = '/0';
+  return bin;
 }
 
 int main() {
@@ -43,4 +31,5 @@ int main() {
 	bin = convert_2(n);
 	printf("The Binary Notation of %d is\t %s\n", n, bin);
 	//TODO: do we need to release the memory of bin?
+  free (bin);
 }
