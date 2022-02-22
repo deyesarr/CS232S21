b29fa2852b6e3671934415b0743d38470d6e3512
Ndeye Sarr <69664183+deyesarr@users.noreply.github.com>
2022-02-22 01:29:18 +0000

    task2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -6,18 +6,32 @@
 char* convert_2(int dec)
 {
 	//TODO: your implementation
-  char* con = (char* )malloc(sizeof(char)*50);
-  int first = 0x80000000;
-  int i=0,j;
-  con[i++] = '0';
-  con[i++] = 'b';
-  for(j=0;j<33;j++) {
-      if(j%4 == 0 && j > 0) con[i++] = ' ';
-      if(dec & first) con[i++] = '1';
-      else con[i++] = '0';
-      dec <<= 1;
-  }
-  return (con);
+ int arr= 32;
+    char *con;
+    con = (char*)malloc((arr+9)*sizeof(char)); 
+    con[0] = '0';
+    con[1] = 'b';
+    int located = arr + 8; //(arr+9)-1
+    int i = 1;
+
+    while(located >= 2)
+    {
+        if(i == 0 ) { 
+                con[located] = ' ';
+                }
+                else {
+                if(dec&1) {  
+                                b[located] ='1';
+                        }
+                        else {
+                                b[located] ='0';
+                        }
+                dec >>= 1;        
+                } 
+            located--; 
+            i = (i+1)%5; 
+    }
+    return con;
 }
 
 int main() {
