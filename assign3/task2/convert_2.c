58168f3e014e127e2e883e03469b28952a7db39b
Ndeye Sarr <69664183+deyesarr@users.noreply.github.com>
2022-02-22 01:44:50 +0000

    task 2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -6,21 +6,30 @@
 char* convert_2(int dec)
 {
 	//TODO: your implementation
-int i, con, init;
-  char *bin;
-  init = 0;
-  bin = (char*)malloc(32+1);
-
-  for (i = 31; i >= 0; i--){
-    con = dec >> i;
-    if (con & 1)
-      *(bin + init) = 1 + '0';
-    else
-      *(bin + init) = 0 + '0';
-    init++;
-  }
-  *(bin + init) = '/0';
-  return bin;
+char* bin = (char*) malloc(50 * sizeof(char));
+    int i = 49;
+    if(dec & 0x80000000) {
+        dec = -1 * dec;
+        while(i >= 0) {
+            if(i % 5 == 0) {
+                bin[i--] = ' ';
+            }
+            bin[i--] = '0' + !(dec%2);
+            dec = dec / 2;
+        }
+        bin[49] = '1';
+    }
+    else {  
+        while(i >= 0) {
+            if(i % 5 == 0) {
+                bin[i--] = ' ';
+            }
+            bin[i--] = '0' + (dec%2);
+            dec = dec / 2;
+        }
+    }
+    bin[50] = '\0';
+    return bin;
 }
 
 int main() {
