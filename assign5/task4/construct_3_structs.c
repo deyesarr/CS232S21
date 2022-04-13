689bcd58a6cfc790bca7683f4bd4373a08db49e4
Ndeye Sarr <69664183+deyesarr@users.noreply.github.com>
2022-04-13 20:50:31 +0000

    completed

--- assign5/task4/construct_3_structs.c
+++ assign5/task4/construct_3_structs.c
@@ -9,10 +9,27 @@ node_t * setup() {
     //Allocate three more pointers
     //head for the first Node, and temporary pointers node1, node2 and node3
     //Allocate three node pointees and store references to them in the three pointers
+    node_t * firstNode = (struct snode*)malloc(sizeof(node_t));
+    node_t * secondNode = (struct snode*)malloc(sizeof(node_t));
+    node_t * thirdNode = (struct snode*)malloc(sizeof(node_t));
     //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
     //Dereference each pointer to access the .next field in its pointee, 
     //and use pointer assignment to set the .next field to point to the appropriate Node. 
-    
+
+  strcpy(firstNode->str, "hello");
+    firstNode->length = 5;//string length
+    firstNode->next = secondNode;//pointer to next node
+
+    strcpy(secondNode->str, "there");
+    secondNode->length = 5;//string length
+    secondNode->next = thirdNode;//pointer to next node
+
+    strcpy(thirdNode->str, "prof");
+    thirdNode->length = 4;//string length
+    thirdNode->next = NULL;//pointer to nonexistent next node
+
+    head = firstNode;// initialize head node
+  
    return head;
 }
 
@@ -20,22 +37,30 @@ void teardown(/*what parameter?*/) {
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
+   node_t *cur = head;
+    while (head != NULL)
+    {
+        // Linked List implementation
+        cur = head->next;
+        free(head);
+        head = cur;
+    }
 }
 
 void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(head);
     return 0;
 }
 
 void dump_all(node_t * head) {
     printf("head -> ");
-    node_t * cur = head;
-    while(cur != NULL) {
-        printf("%s ", cur->str);
-	cur = cur->next;
+    node_t * selected = head;
+    while(selected != NULL) {
+        printf("%s ", selected->str);
+	      selected = selected->next;
     }
-    printf("\ndone\n ");
+    printf("\nComplete\n ");
 }
