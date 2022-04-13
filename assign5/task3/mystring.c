566f3e6d2c616508795a44fae83a2aa7103cbe57
Ndeye Sarr <69664183+deyesarr@users.noreply.github.com>
2022-04-13 20:30:57 +0000

    completed

--- assign5/task3/mystring.c
+++ assign5/task3/mystring.c
@@ -61,37 +61,66 @@ mystring_t also_bad_string_new() {
 mystring_t *mystring_new() {
 	mystring_t *retval;
 	//YOUR CODE HERE
+retval = malloc(sizeof(mystring_t*));
+	retval->size = 1;
+  retval->data = 0;
+  
 	return retval;
 }
 
 /* Return the value at the specified location/component "loc" of the mystring */
 char mystring_get(mystring_t *s, size_t loc) {
 	//YOUR CODE HERE
-	return ' ';
+   if(loc>=0 && loc < s->size){
+        return s->data[loc];
+    }
+    else{
+        return 0;
+    }
 }
 
 /* Free up the memory allocated for the passed mystring.
    Remember, you need to free up ALL the memory that was allocated. */
 void mystring_delete(mystring_t *s) {
 	/* YOUR CODE HERE */
+  free(s->data);
+    free(s); 
+  //free pointer then free variable 
 }
 
 int mystring_get_len(mystring_t *s) {
 	/* YOUR CODE HERE */
+  return s->size;
 }
 
 char* mystring_get_data(mystring_t *s) {
 	/* YOUR CODE HERE*/
 	return s->data;
 }
+
 void mystring_cat(mystring_t *s, char *s2) {
 	/* YOUR CODE HERE*/
+  int i=0,x;
+    while(s->data[i]!= '\0'){ //gets the last index of the list before NULL
+        i++;
+    }
+    
+    for(x=0; s2[x]!='\0'; x++, i++)
+    {
+       s->data[i]=s2[x];
+    }
+    s->data[i]='\0';
 }
 
 /* Set a value in the mystring. If the extra memory allocation fails, call
    allocation_failed(). Unset space should be value of space */
 void mystring_set(mystring_t *s, size_t loc, char value) {
 	/* YOUR CODE HERE*/
-
+ if(loc > s->size){
+           allocation_failed();
+        } else {
+           s->data = realloc(s->data, sizeof(s->data) + 1);
+           s->data[loc] = value;
+        }
 	return;
 }
